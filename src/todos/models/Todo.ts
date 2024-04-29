083fce49c8177735b4aa31a84e9637cefe261b73
will-oracions <oracions.dev@gmail.com>
2024-04-29 22:45:51 +0100

    create Todo, Assignee, TodoPrority and TodoLabel interface and enums

--- src/todos/models/Todo.ts
+++ src/todos/models/Todo.ts
@@ -1,3 +1,29 @@
 export interface Todo {
   id: number;
+  title: string;
+  assignee: Assignee;
+  startDate: Date;
+  endDate: Date;
+  prority: TodoPrority;
+  labels: TodoLabel[];
+  description: string;
+}
+
+export interface Assignee {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export enum TodoPrority {
+  LOW,
+  MEDIUM,
+  HIGHT,
+}
+
+export enum TodoLabel {
+  HTML,
+  CSS,
+  JQUERY,
+  NODEJS,
 }
