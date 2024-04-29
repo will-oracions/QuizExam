ce811241eb5e4b3457360784b0250ec0f643cf60
will-oracions <oracions.dev@gmail.com>
2024-04-29 12:46:55 +0100

    init the project and setup node-sass, docker, docker-compose

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:20-alpine
+
+WORKDIR /app
+
+RUN npm install -g pnpm
+
+COPY package.json pnpm-lock.yaml ./
+
+RUN pnpm install
+
+COPY . .
+
+RUN pnpm run build
+
+EXPOSE 7777
+
+CMD ["pnpm", "start:dev"]
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1>WizeTodoList</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea at
+          distinctio, vel, quia ab iste optio dolores doloribus explicabo sed
+          recusandae voluptate beatae excepturi rerum mollitia sequi velit
+          blanditiis. Magnam.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
