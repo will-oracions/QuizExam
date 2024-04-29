ebc3e6bbe9b87fe9a59aaccb7424042255304acd
will-oracions <oracions.dev@gmail.com>
2024-04-29 14:36:39 +0100

    create api mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
+import React from "react";
+import axios from "axios";
+
 import Header from "./components/Header";
 
+import "./api/mockAxios";
+import { ITodo } from "./todos/ITodo";
+
 function App() {
+  const [todos, setTodos] = React.useState<ITodo[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      await axios.post("/todos", { id: 997 });
+      await axios.post("/todos", { id: 888 });
+
+      await axios.delete("/todos/888");
+      await axios.delete("/todos/1");
+
+      const res = await axios.get("/todos");
+      console.log("Todos: ", res.data);
+      setTodos(res.data);
+    })();
+  }, []);
+
   return (
     <>
       <div className="container">
         <Header />
         <h1>WizeTodoList</h1>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>{todo.id}</li>
+          ))}
+        </ul>
         <p>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea at
           distinctio, vel, quia ab iste optio dolores doloribus explicabo sed
---
+++ src/api/mockAxios.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { ITodo } from "../todos/ITodo";
+
+const mock = new MockAdapter(axios);
+
+let todos: ITodo[] = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+
+mock.onGet("/todos").reply(200, todos);
+
+mock.onPost("/todos").reply((config) => {
+  const todo = JSON.parse(config.data);
+  todos.push(todo);
+  return [201, todo];
+});
+
+mock.onPut(/\/todos\/\d+/).reply((config) => {
+  const todoId = parseInt(config.url!.split("/").pop()!);
+  const updatedTodo: ITodo = JSON.parse(config.data);
+  todos = todos.map((todo) => (todo.id === todoId ? updatedTodo : todo));
+  return [200, updatedTodo];
+});
+
+mock.onDelete(/\/todos\/\d+/).reply((config) => {
+  const todoId = parseInt(config.url!.split("/").pop()!);
+  todos = todos.filter((todo) => todo.id !== todoId);
+  return [204];
+});
+
+export default mock;
