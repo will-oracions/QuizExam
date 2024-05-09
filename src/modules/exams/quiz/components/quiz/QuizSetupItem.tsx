7f4990815873c21e1c998ee47232babfddac6da0
will-oracions <oracions.dev@gmail.com>
2024-05-09 09:09:32 +0100

    save old

--- src/modules/exams/quiz/components/quiz/QuizSetupItem.tsx
+++ src/modules/exams/quiz/components/quiz/QuizSetupItem.tsx
@@ -1,31 +1,38 @@
 import { Box, Typography } from "@mui/material";
 import QuizSetupInput from "./QuizSetupInput";
 import QuizSetupItemHeader from "./QuizSetupItemHeader";
+import React from "react";
 
 interface Props {
   children?: React.ReactNode;
 }
 
 const QuizSetupItem = ({ children }: Props) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <>
       <Box className="app-quiz-setup-section">
-        <QuizSetupItemHeader />
-
-        <Box className="app-quiz-question-section-content">
-          <Box>
-            <Box className="app-quiz-question-text">
-              <Typography>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptate a earum doloribus vel hic quae nulla, doloremque quos
-                harum ipsam?
-              </Typography>
-
-              <QuizSetupInput />
-            </Box>
-          </Box>
-          {children}
+        <Box onClick={() => setIsOpen(!isOpen)}>
+          <QuizSetupItemHeader />
         </Box>
+
+        {isOpen && (
+          <Box className="app-quiz-question-section-content">
+            <Box>
+              <Box className="app-quiz-question-text">
+                <Typography>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptate a earum doloribus vel hic quae nulla, doloremque
+                  quos harum ipsam?
+                </Typography>
+
+                <QuizSetupInput />
+              </Box>
+            </Box>
+            {children}
+          </Box>
+        )}
       </Box>
     </>
   );
